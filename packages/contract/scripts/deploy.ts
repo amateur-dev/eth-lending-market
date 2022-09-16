a5dd5facd7ef35b6c91ac2b357a85db34a179842
amateur_dev <me@dipeshsukhani.dev>
2022-09-16 10:28:16 +0800

    WIP

--- packages/contract/scripts/deploy.ts
+++ packages/contract/scripts/deploy.ts
@@ -1,7 +1,7 @@
 import { ethers } from "hardhat";
 import { formatUnits, parseUnits } from "ethers/lib/utils";
 import { assert } from "chai";
-import { deployErc20Token, Erc20Token } from "@dany-armstrong/hardhat-erc20";
+import { deployErc20Token, Erc20Token,Erc20Token__factory } from "@dany-armstrong/hardhat-erc20";
 import {
   CTokenDeployArg,
   deployCompoundV2,
@@ -12,55 +12,64 @@ import { join } from 'path';
 
 async function main() {
   const [deployer] = await ethers.getSigners();
-  const UNI_PRICE = "25022748000000000000";
-  const USDC_PRICE = "1000000000000000000";
+
+  //TODO: to check why this is needed
+  //TODO: to fetch the price from the mainnet Uniswap contract
+  const UNI_PRICE = uniswap.routerMainnet.getPrice //
+  const UNI_PRICE = "25022748000000000000"; // UniPrice
+  const USDC_PRICE = "1000000000000000000"; 
   const AAVE_PRICE = "92500000000000000000";
   const DAI_PRICE = "1000000000000000000";
   const ETH_PRICE = "1738000000000000000000";
 
   // Deploy USDC ERC20
-  const USDC: Erc20Token = await deployErc20Token(
-      {
-        name: "USD Coin",
-        symbol: "USDC",
-        decimals: 6,
-      },
-      deployer
-  );
-  console.log('USDC token deployed!');
+  const USDC = Erc20Token__factory.connect("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",deployer); // mainnet address
+  const UNI = Erc20Token__factory.connect("PARI TO INSERT",erc20abi,deployer)
+  const AAVE = Erc20Token__factory.connect("PARI TO INSERT",erc20abi,deployer)
+  const DAI = Erc20Token__factory.connect("PARI TO INSERT",erc20abi,deployer)
+  
+  // const USDC: Erc20Token = await deployErc20Token(
+  //     {
+  //       name: "USD Coin",
+  //       symbol: "USDC",
+  //       decimals: 6,
+  //     },
+  //     deployer
+  // );
+  // console.log('USDC token deployed!');
 
   // Deploy UNI ERC20
-  const UNI: Erc20Token = await deployErc20Token(
-      {
-        name: "Uniswap",
-        symbol: "UNI",
-        decimals: 18,
-      },
-      deployer
-  );
-  console.log('UNI token deployed!');
+  // const UNI: Erc20Token = await deployErc20Token(
+  //     {
+  //       name: "Uniswap",
+  //       symbol: "UNI",
+  //       decimals: 18,
+  //     },
+  //     deployer
+  // );
+  // console.log('UNI token deployed!');
 
   // Deploy AAVE ERC20
-  const AAVE: Erc20Token = await deployErc20Token(
-      {
-        name: "Aave Token",
-        symbol: "AAVE",
-        decimals: 18,
-      },
-      deployer
-  );
-  console.log('AAVE token deployed!');
+  // const AAVE: Erc20Token = await deployErc20Token(
+  //     {
+  //       name: "Aave Token",
+  //       symbol: "AAVE",
+  //       decimals: 18,
+  //     },
+  //     deployer
+  // );
+  // console.log('AAVE token deployed!');
 
   // Deploy DAI ERC20
-  const DAI: Erc20Token = await deployErc20Token(
-      {
-        name: "Dai Token",
-        symbol: "DAI",
-        decimals: 18,
-      },
-      deployer
-  );
-  console.log('DAI token deployed!');
+  // const DAI: Erc20Token = await deployErc20Token(
+  //     {
+  //       name: "Dai Token",
+  //       symbol: "DAI",
+  //       decimals: 18,
+  //     },
+  //     deployer
+  // );
+  // console.log('DAI token deployed!');
 
   const ctokenArgs: CTokenDeployArg[] = [
     {
@@ -95,7 +104,7 @@ async function main() {
   ];
 
   const { comptroller, cTokens, priceOracle, interestRateModels } =
-      await deployCompoundV2(ctokenArgs, deployer);
+      await deployCompoundV2(ctokenArgs, deployer); //@pari do we really need to do it?
 
   var tx = await comptroller._setCloseFactor(parseUnits("0.5", 18).toString());
   await tx.wait();
